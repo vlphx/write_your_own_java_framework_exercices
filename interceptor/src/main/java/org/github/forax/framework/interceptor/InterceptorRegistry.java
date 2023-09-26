f13085ebbb365b8b2062416083ade9e6cc8cd49b
Vivien <alphonsevivien@gmail.com>
2023-09-26 11:26:36 +0200

    (LAB3 - Interceptor) Q5 - change the implementation to only uses interceptor internally

--- interceptor/src/main/java/org/github/forax/framework/interceptor/InterceptorRegistry.java
+++ interceptor/src/main/java/org/github/forax/framework/interceptor/InterceptorRegistry.java
@@ -10,20 +10,34 @@ import java.util.List;
 import java.util.Objects;
 
 public final class InterceptorRegistry {
-    private final HashMap<Class<? extends Annotation>, List<AroundAdvice>> aroundAdvicesByAnnotationMap = new HashMap<>();
+    // private final HashMap<Class<? extends Annotation>, List<AroundAdvice>> aroundAdvicesByAnnotationMap = new HashMap<>();
     private final HashMap<Class<? extends Annotation>, List<Interceptor>> interceptorsByAnnotationMap = new HashMap<>();
 
-    List<AroundAdvice> findAdvices(Method method) {
-        Objects.requireNonNull(method);
-        return Arrays
-                .stream(method.getAnnotations())
-                .flatMap(annotation -> aroundAdvicesByAnnotationMap.getOrDefault(annotation.annotationType(), List.of()).stream())
-                .toList();
-    }
+//    List<AroundAdvice> findAdvices(Method method) {
+//        Objects.requireNonNull(method);
+//        return Arrays
+//                .stream(method.getAnnotations())
+//                .flatMap(annotation -> aroundAdvicesByAnnotationMap.getOrDefault(annotation.annotationType(), List.of()).stream())
+//                .toList();
+//    }
     public void addAroundAdvice(Class<? extends Annotation> annotationClass, AroundAdvice aroundAdvice) {
         Objects.requireNonNull(annotationClass);
         Objects.requireNonNull(aroundAdvice);
-        aroundAdvicesByAnnotationMap.computeIfAbsent(annotationClass, __ -> new ArrayList<>()).add(aroundAdvice);
+//        aroundAdvicesByAnnotationMap.computeIfAbsent(annotationClass, __ -> new ArrayList<>()).add(aroundAdvice);
+        addInterceptor(annotationClass,
+                ((instance, method, args, invocation) -> {
+                    aroundAdvice.before(instance, method, args);
+                    Object result = null;
+                    // var invocation = new Interceptor().intercept(instance, method, args, aroundAdvice.after);
+                    try{
+                        return result = invocation.proceed(instance, method, args);
+                    }
+                    finally {
+                        aroundAdvice.after(instance, method, args, result);
+                    }
+                })
+                );
+
     }
 
     public <T> T createProxy(Class<T> interfaceType, T instance) {
@@ -31,19 +45,22 @@ public final class InterceptorRegistry {
         Objects.requireNonNull(instance);
         return interfaceType.cast(Proxy.newProxyInstance(interfaceType.getClassLoader(), new Class[]{interfaceType},
                 (Object __, Method method, Object[] args) -> {
-                    var advices = findAdvices(method);
-                    for (var advice : advices){
-                        advice.before(instance, method, args);
-                    }
-                    Object result = null;
-                    try {
-                        result = Utils.invokeMethod(instance, method, args);
-                        return result;
-                    } finally {
-                        for(var advice : advices.reversed()) {
-                            advice.after(instance, method, args, result);
-                        }
-                    }
+//                    var advices = findAdvices(method);
+//                    for (var advice : advices){
+//                        advice.before(instance, method, args);
+//                    }
+//                    Object result = null;
+//                    try {
+//                        result = Utils.invokeMethod(instance, method, args);
+//                        return result;
+//                    } finally {
+//                        for(var advice : advices.reversed()) {
+//                            advice.after(instance, method, args, result);
+//                        }
+//                    }
+                    var interceptors = findInterceptors(method);
+                    var invocation = getInvocation(interceptors);
+                    return invocation.proceed(instance, method, args);
                 })
         );
     }
