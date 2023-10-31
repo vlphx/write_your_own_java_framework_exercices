203b1e9e2bade58d1e2f53cd1da3d5001f7f56b1
Vivien <alphonsevivien@gmail.com>
2023-10-31 10:59:53 +0100

    (LAB5 - ORM - JSON to Object) Q1 - Q4

--- mapper/src/main/java/com/github/forax/framework/mapper/JSONReader.java
+++ mapper/src/main/java/com/github/forax/framework/mapper/JSONReader.java
@@ -2,63 +2,129 @@ package com.github.forax.framework.mapper;
 
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Type;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class JSONReader {
-  private record BeanData(Constructor<?> constructor, Map<String, PropertyDescriptor> propertyMap) {
-    PropertyDescriptor findProperty(String key) {
-      var property = propertyMap.get(key);
-      if (property == null) {
-        throw new IllegalStateException("unknown key " + key + " for bean " + constructor.getDeclaringClass().getName());
-      }
-      return property;
+    private record BeanData(Constructor<?> constructor, Map<String, PropertyDescriptor> propertyMap) {
+        PropertyDescriptor findProperty(String key) {
+            var property = propertyMap.get(key);
+            if (property == null) {
+                throw new IllegalStateException("unknown key " + key + " for bean " + constructor.getDeclaringClass().getName());
+            }
+            return property;
+        }
     }
-  }
 
-  private static final ClassValue<BeanData> BEAN_DATA_CLASS_VALUE = new ClassValue<>() {
-    @Override
-    protected BeanData computeValue(Class<?> type) {
-      throw new UnsupportedOperationException("TODO");
-    }
-  };
-
-  public Object parseJSON(String text, Class<?> beanClass) {
-    Objects.requireNonNull(text);
-    Objects.requireNonNull(beanClass);
-    var visitor = new ToyJSONParser.JSONVisitor() {
-      private BeanData beanData;
-      private Object result;
-
-      @Override
-      public void value(String key, Object value) {
-        // call the corresponding setter on result
-        throw new UnsupportedOperationException("TODO");
-      }
-
-      @Override
-      public void startObject(String key) {
-        //get the beanData and store it in the field
-        //create an instance and store it in result
-        throw new UnsupportedOperationException("TODO");
-      }
-
-      @Override
-      public void endObject(String key) {
-        // do nothing
-      }
-
-      @Override
-      public void startArray(String key) {
-        throw new UnsupportedOperationException("Implemented later");
-      }
-
-      @Override
-      public void endArray(String key) {
-        throw new UnsupportedOperationException("Implemented later");
-      }
+    private static final ClassValue<BeanData> BEAN_DATA_CLASS_VALUE = new ClassValue<>() {
+        @Override
+        protected BeanData computeValue(Class<?> type) {
+            // throw new UnsupportedOperationException("TODO");
+            var beanInfo = Utils.beanInfo(type);
+            var map = Arrays.stream(beanInfo.getPropertyDescriptors())
+                    .filter(property -> !property.getName().equals("class"))
+                    .collect(Collectors.toMap(PropertyDescriptor::getName, Function.identity())); // Function.identity() is the same as property -> property
+            var constructor = Utils.defaultConstructor(type);
+            return new BeanData(constructor, map);
+        }
     };
-    ToyJSONParser.parse(text, visitor);
-    return visitor.result;
-  }
+
+    public record ObjectBuilder<T>(Function<? super String, ? extends Type> typeProvider,
+                                   Supplier<? extends T> supplier,
+                                   Populater<? super T> populater,
+                                   Function<? super T, ?> finisher) {
+        public interface Populater<T> {
+            void populate(T instance, String key, Object value);
+        }
+
+        public static ObjectBuilder<Object> bean(Class<?> beanClass) {
+            Objects.requireNonNull(beanClass);
+            var beanData = BEAN_DATA_CLASS_VALUE.get(beanClass);
+            var constructor = beanData.constructor;
+            return new ObjectBuilder<>(
+                    key -> beanData.findProperty(key).getWriteMethod().getGenericParameterTypes()[0],
+                    () -> Utils.newInstance(constructor),
+                    (instance, key, value) -> {
+                        var setter = beanData.findProperty(key).getWriteMethod();
+                        Utils.invokeMethod(instance, setter, value);
+                    },
+                    Function.identity()
+            );
+        }
+    }
+
+
+    private record Context(ObjectBuilder<Object> objectBuilder, Object result) {
+    }
+
+    public <T> T parseJSON(String text, Class<T> beanClass) {
+        return beanClass.cast(parseJSON(text, (Type) beanClass));
+    }
+
+    public Object parseJSON(String text, Type expectedType) {
+        Objects.requireNonNull(text);
+        Objects.requireNonNull(expectedType);
+
+        var stack = new ArrayDeque<Context>(); // anonymous class below can capture this variable (all local variables by the way)
+        var visitor = new ToyJSONParser.JSONVisitor() {
+            private BeanData beanData;
+            private Object result;
+
+            @Override
+            public void value(String key, Object value) {
+                var currentContext = stack.peek();
+//                var setter = Objects.requireNonNull(currentContext).beanData.findProperty(key).getWriteMethod();
+//                Utils.invokeMethod(currentContext.result, setter, value);
+                currentContext.objectBuilder.populater.populate(currentContext.result, key, value);
+            }
+
+            @Override
+            public void startObject(String key) {
+                var currentContext = stack.peek();
+                var beanType = currentContext == null ?
+                        expectedType : currentContext.objectBuilder.typeProvider.apply(key);
+                var objectBuilder = ObjectBuilder.bean(Utils.erase(beanType));
+                var instance = objectBuilder.supplier.get();
+
+//                beanData = BEAN_DATA_CLASS_VALUE.get(beanType);
+                //get the beanData and store it in the field
+                //create an instance and store it in result
+//                var instance = Utils.newInstance(beanData.constructor);
+                stack.push(new Context(objectBuilder, instance));
+            }
+
+            @Override
+            public void endObject(String key) {
+                var previoustContext = stack.pop();
+                var result = previoustContext.result;
+                if (stack.isEmpty())
+                    this.result = result;
+                else {
+                    var currentContext = stack.peek();
+//                    var setter = currentContext.objectBuilder.finisher.apply(currentContext.result);
+//                            .beanData.findProperty(key).getWriteMethod();
+//                    Utils.invokeMethod(currentContext.result, setter, result);
+                    currentContext.objectBuilder.populater.populate(currentContext.result, key, result);
+                }
+            }
+
+            @Override
+            public void startArray(String key) {
+                throw new UnsupportedOperationException("Implemented later");
+            }
+
+            @Override
+            public void endArray(String key) {
+                throw new UnsupportedOperationException("Implemented later");
+            }
+        };
+        ToyJSONParser.parse(text, visitor);
+        return visitor.result;
+    }
 }
--- mapper/src/test/java/com/github/forax/framework/mapper/JSONReaderTest.java
+++ mapper/src/test/java/com/github/forax/framework/mapper/JSONReaderTest.java
@@ -8,289 +8,301 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
-import java.util.StringJoiner;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class JSONReaderTest {
-  /*
-  @Nested
-  public class Q1 {
 
-    @SuppressWarnings("unused")
-    public static class SimpleBean {
-      private String name;
-      private int age;
+    @Nested
+    public class Q1 {
 
-      public void setName(String name) {
-        this.name = name;
-      }
-      public void setAge(int age) {
-        this.age = age;
-      }
-    }
+        @SuppressWarnings("unused")
+        public static class SimpleBean {
+            private String name;
+            private int age;
 
-    @Test @Tag("Q1")
-    public void parseJSON() {
-      var reader = new JSONReader();
-      SimpleBean bean = reader.parseJSON("""
-        {
-          "name": "Bob",
-          "age": 23
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public void setAge(int age) {
+                this.age = age;
+            }
         }
-        """, SimpleBean.class);
-      assertAll(
-          () -> assertEquals("Bob", bean.name),
-          () -> assertEquals(23, bean.age)
-      );
-    }
 
-    @Test @Tag("Q1")
-    public void parseJSONPartial() {
-      var reader = new JSONReader();
-      var bean = reader.parseJSON("""
-        {
-          "name": "Bob"
+        @Test
+        @Tag("Q1")
+        public void parseJSON() {
+            var reader = new JSONReader();
+            SimpleBean bean = reader.parseJSON("""
+                    {
+                      "name": "Bob",
+                      "age": 23
+                    }
+                    """, SimpleBean.class);
+            assertAll(
+                    () -> assertEquals("Bob", bean.name),
+                    () -> assertEquals(23, bean.age)
+            );
         }
-        """, SimpleBean.class);
-      assertEquals("Bob", bean.name);
-      assertEquals(0, bean.age);
-    }
 
-    @Test @Tag("Q1")
-    public void parseJSONEmpty() {
-      var reader = new JSONReader();
-      var bean = reader.parseJSON("""
-        {}
-        """, SimpleBean.class);
-      assertNull(bean.name);
-      assertEquals(0, bean.age);
-    }
-
-    @SuppressWarnings("unused")
-    public static class PrimitiveBean {
-      private Void key1;
-      private boolean key2;
-      private boolean key3;
-      private int key4;
-      private double key5;
-      private String key6;
-
-      private final HashSet<String> keys = new HashSet<>();
-
-      public void setKey1(Void key1) {
-        this.key1 = key1;
-        keys.add("key1");
-      }
-      public void setKey2(boolean key2) {
-        this.key2 = key2;
-        keys.add("key2");
-      }
-      public void setKey3(boolean key3) {
-        this.key3 = key3;
-        keys.add("key3");
-      }
-      public void setKey4(int key4) {
-        this.key4 = key4;
-        keys.add("key4");
-      }
-      public void setKey5(double key5) {
-        this.key5 = key5;
-        keys.add("key5");
-      }
-      public void setKey6(String key6) {
-        this.key6 = key6;
-        keys.add("key6");
-      }
-    }
-
-    @Test @Tag("Q1")
-    public void parseJSONPrimitive() {
-      var reader = new JSONReader();
-      var bean = reader.parseJSON("""
-        {
-          "key1": null,
-          "key2": false,
-          "key3": true,
-          "key4": 12,
-          "key5": 144.4,
-          "key6": "string"
+        @Test
+        @Tag("Q1")
+        public void parseJSONPartial() {
+            var reader = new JSONReader();
+            var bean = reader.parseJSON("""
+                    {
+                      "name": "Bob"
+                    }
+                    """, SimpleBean.class);
+            assertEquals("Bob", bean.name);
+            assertEquals(0, bean.age);
         }
-        """, PrimitiveBean.class);
-      assertAll(
-          () -> assertEquals(Set.of("key1", "key2", "key3", "key4", "key5", "key6"), bean.keys),
-          () -> assertNull(bean.key1),
-          () -> assertFalse(bean.key2),
-          () -> assertTrue(bean.key3),
-          () -> assertEquals(12, bean.key4),
-          () -> assertEquals(144.4, bean.key5),
-          () -> assertEquals("string", bean.key6)
-      );
-    }
 
-    @Test @Tag("Q1")
-    public void parseJSONInvalidKey() {
-      var reader = new JSONReader();
-      var exception = assertThrows(IllegalStateException.class, () -> {
-        reader.parseJSON("""
-        {
-          "invalidKey": "oops"
+        @Test
+        @Tag("Q1")
+        public void parseJSONEmpty() {
+            var reader = new JSONReader();
+            var bean = reader.parseJSON("""
+                    {}
+                    """, SimpleBean.class);
+            assertNull(bean.name);
+            assertEquals(0, bean.age);
         }
-        """, SimpleBean.class);
-      });
-      assertInstanceOf(IllegalStateException.class, exception.getCause());
-    }
 
-    @Test @Tag("Q1")
-    public void parseJSONClassPrecondition() {
-      var reader = new JSONReader();
-      assertAll(
-          () -> assertThrows(NullPointerException.class, () -> reader.parseJSON(null, String.class)),
-          () -> assertThrows(NullPointerException.class, () -> reader.parseJSON("", (Class<?>) null))
-      );
-    }
+        @SuppressWarnings("unused")
+        public static class PrimitiveBean {
+            private Void key1;
+            private boolean key2;
+            private boolean key3;
+            private int key4;
+            private double key5;
+            private String key6;
 
-  }  // end of Q1
+            private final HashSet<String> keys = new HashSet<>();
 
+            public void setKey1(Void key1) {
+                this.key1 = key1;
+                keys.add("key1");
+            }
 
-  @Nested
-  public class Q2 {
+            public void setKey2(boolean key2) {
+                this.key2 = key2;
+                keys.add("key2");
+            }
 
-    @SuppressWarnings("unused")
-    public static class Person {
-      private Address address;
+            public void setKey3(boolean key3) {
+                this.key3 = key3;
+                keys.add("key3");
+            }
 
-      public void setAddress(Address address) {
-        this.address = address;
-      }
-    }
-    @SuppressWarnings("unused")
-    public static class Address {
-      private String zipCode;
+            public void setKey4(int key4) {
+                this.key4 = key4;
+                keys.add("key4");
+            }
 
-      public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-      }
-    }
+            public void setKey5(double key5) {
+                this.key5 = key5;
+                keys.add("key5");
+            }
 
-    @Test @Tag("Q2")
-    public void parseJSONRecursive() {
-      var reader = new JSONReader();
-      var person = reader.parseJSON("""
-        {
-          "address": {
-            "zipCode": "75001"
-          }
+            public void setKey6(String key6) {
+                this.key6 = key6;
+                keys.add("key6");
+            }
         }
-        """, Person.class);
-      assertEquals("75001", person.address.zipCode);
-    }
 
-    @Test @Tag("Q2")
-    public void parseJSONRecursiveInvalidKey() {
-      var reader = new JSONReader();
-      assertThrows(IllegalStateException.class, () -> {
-            reader.parseJSON("""
-                {
-                  "invalidAddress": {
-                    "zipCode": "75001"
-                  }
-                }
-                """, Person.class);
-          });
-    }
+        @Test
+        @Tag("Q1")
+        public void parseJSONPrimitive() {
+            var reader = new JSONReader();
+            var bean = reader.parseJSON("""
+                    {
+                      "key1": null,
+                      "key2": false,
+                      "key3": true,
+                      "key4": 12,
+                      "key5": 144.4,
+                      "key6": "string"
+                    }
+                    """, PrimitiveBean.class);
+            assertAll(
+                    () -> assertEquals(Set.of("key1", "key2", "key3", "key4", "key5", "key6"), bean.keys),
+                    () -> assertNull(bean.key1),
+                    () -> assertFalse(bean.key2),
+                    () -> assertTrue(bean.key3),
+                    () -> assertEquals(12, bean.key4),
+                    () -> assertEquals(144.4, bean.key5),
+                    () -> assertEquals("string", bean.key6)
+            );
+        }
 
-  }  // end of Q2
+        @Test
+        @Tag("Q1")
+        public void parseJSONInvalidKey() {
+            var reader = new JSONReader();
+            var exception = assertThrows(IllegalStateException.class, () -> {
+                reader.parseJSON("""
+                        {
+                          "invalidKey": "oops"
+                        }
+                        """, SimpleBean.class);
+            });
+            assertInstanceOf(IllegalStateException.class, exception.getCause());
+        }
 
+        @Test
+        @Tag("Q1")
+        public void parseJSONClassPrecondition() {
+            var reader = new JSONReader();
+            assertAll(
+                    () -> assertThrows(NullPointerException.class, () -> reader.parseJSON(null, String.class)),
+                    () -> assertThrows(NullPointerException.class, () -> reader.parseJSON("", (Class<?>) null))
+            );
+        }
 
-  @Nested
-  public class Q3 {
-
-    @SuppressWarnings("unused")
-    public static class Person {
-      private String name;
-      private int age;
-
-      public void setName(String name) {
-        this.name = name;
-      }
-      public void setAge(int age) {
-        this.age = age;
-      }
-    }
-
-    @Test @Tag("Q3")
-    public void objectBuilderBean() {
-      JSONReader.ObjectBuilder<Object> objectBuilder = JSONReader.ObjectBuilder.bean(Person.class);
-      var bean = objectBuilder.supplier().get();
-      objectBuilder.populater().populate(bean, "name", "Bob");
-      objectBuilder.populater().populate(bean, "age", 29);
-      var person = (Person) objectBuilder.finisher().apply(bean);
-
-      assertAll(
-          () -> assertEquals("Bob", person.name),
-          () -> assertEquals(29, person.age)
-      );
-    }
-
-    @Test @Tag("Q3")
-    public void objectBuilderBeanTypeProvider() {
-      var objectBuilder = JSONReader.ObjectBuilder.bean(Person.class);
-      assertAll(
-          () -> assertEquals(String.class, objectBuilder.typeProvider().apply("name")),
-          () -> assertEquals(int.class, objectBuilder.typeProvider().apply("age"))
-      );
-    }
-
-    @Test @Tag("Q3")
-    public void objectBuilderBeanPreconditions() {
-      assertThrows(NullPointerException.class, () -> JSONReader.ObjectBuilder.bean(null));
-    }
-
-  }  // end of Q3
+    }  // end of Q1
 
 
-  @Nested
-  class Q4 {
-    @Test @Tag("Q4")
-    public void parseJSONTypePrecondition() {
-      var reader = new JSONReader();
-      assertAll(
-          () -> assertThrows(NullPointerException.class, () -> reader.parseJSON(null, (Type) String.class)),
-          () -> assertThrows(NullPointerException.class, () -> reader.parseJSON("", (Type) null))
-      );
-    }
+    @Nested
+    public class Q2 {
 
-    @SuppressWarnings("unused")
-    public static class PreciseTypeBean {
-      public void setFoo(List<String> list) {}
-    }
+        @SuppressWarnings("unused")
+        public static class Person {
+            private Address address;
 
-    @Test @Tag("Q4")
-    public void objectBuilderBeanQualifierPreciseType() {
-      var objectBuilder = JSONReader.ObjectBuilder.bean(PreciseTypeBean.class);
-      var type = objectBuilder.typeProvider().apply("foo");
-      assertAll(
-          () -> assertNotEquals(List.class, type),
-          () -> assertEquals(List.class, ((ParameterizedType) type).getRawType()),
-          () -> assertEquals(String.class, ((ParameterizedType) type).getActualTypeArguments()[0])
-      );
-    }
+            public void setAddress(Address address) {
+                this.address = address;
+            }
+        }
 
-  }  // end of Q4
+        @SuppressWarnings("unused")
+        public static class Address {
+            private String zipCode;
+
+            public void setZipCode(String zipCode) {
+                this.zipCode = zipCode;
+            }
+        }
+
+        @Test
+        @Tag("Q2")
+        public void parseJSONRecursive() {
+            var reader = new JSONReader();
+            var person = reader.parseJSON("""
+                    {
+                      "address": {
+                        "zipCode": "75001"
+                      }
+                    }
+                    """, Person.class);
+            assertEquals("75001", person.address.zipCode);
+        }
+
+        @Test
+        @Tag("Q2")
+        public void parseJSONRecursiveInvalidKey() {
+            var reader = new JSONReader();
+            assertThrows(IllegalStateException.class, () -> {
+                reader.parseJSON("""
+                        {
+                          "invalidAddress": {
+                            "zipCode": "75001"
+                          }
+                        }
+                        """, Person.class);
+            });
+        }
+
+    }  // end of Q2
 
 
+    @Nested
+    public class Q3 {
 
+        @SuppressWarnings("unused")
+        public static class Person {
+            private String name;
+            private int age;
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public void setAge(int age) {
+                this.age = age;
+            }
+        }
+
+        @Test
+        @Tag("Q3")
+        public void objectBuilderBean() {
+            JSONReader.ObjectBuilder<Object> objectBuilder = JSONReader.ObjectBuilder.bean(Person.class);
+            var bean = objectBuilder.supplier().get();
+            objectBuilder.populater().populate(bean, "name", "Bob");
+            objectBuilder.populater().populate(bean, "age", 29);
+            var person = (Person) objectBuilder.finisher().apply(bean);
+
+            assertAll(
+                    () -> assertEquals("Bob", person.name),
+                    () -> assertEquals(29, person.age)
+            );
+        }
+
+        @Test
+        @Tag("Q3")
+        public void objectBuilderBeanTypeProvider() {
+            var objectBuilder = JSONReader.ObjectBuilder.bean(Person.class);
+            assertAll(
+                    () -> assertEquals(String.class, objectBuilder.typeProvider().apply("name")),
+                    () -> assertEquals(int.class, objectBuilder.typeProvider().apply("age"))
+            );
+        }
+
+        @Test
+        @Tag("Q3")
+        public void objectBuilderBeanPreconditions() {
+            assertThrows(NullPointerException.class, () -> JSONReader.ObjectBuilder.bean(null));
+        }
+
+    }  // end of Q3
+
+
+    @Nested
+    class Q4 {
+        @Test
+        @Tag("Q4")
+        public void parseJSONTypePrecondition() {
+            var reader = new JSONReader();
+            assertAll(
+                    () -> assertThrows(NullPointerException.class, () -> reader.parseJSON(null, (Type) String.class)),
+                    () -> assertThrows(NullPointerException.class, () -> reader.parseJSON("", (Type) null))
+            );
+        }
+
+        @SuppressWarnings("unused")
+        public static class PreciseTypeBean {
+            public void setFoo(List<String> list) {
+            }
+        }
+
+        @Test
+        @Tag("Q4")
+        public void objectBuilderBeanQualifierPreciseType() {
+            var objectBuilder = JSONReader.ObjectBuilder.bean(PreciseTypeBean.class);
+            var type = objectBuilder.typeProvider().apply("foo");
+            assertAll(
+                    () -> assertNotEquals(List.class, type),
+                    () -> assertEquals(List.class, ((ParameterizedType) type).getRawType()),
+                    () -> assertEquals(String.class, ((ParameterizedType) type).getActualTypeArguments()[0])
+            );
+        }
+
+    }  // end of Q4
+
+
+/*
   @Nested
   public class Q5 {
     @Test @Tag("Q5")
